8469358491230a869ee1936e9ab2e937c8f81330
Nick Karnik <theoutlander@live.com>
2018-03-16 00:09:39 -0700

    Removed external CSS Added styled components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-// import "./App.css";
-import Counter from "./counter";
-import Notification from "./notification";
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+import Counter from './counter'
+import Notification from './notification'
+import { Responsive, Header, Segment, Grid, Container } from 'semantic-ui-react'
 
 //const App = () => "TEST"
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div>
-        <Notification />
-        <Counter seed={10} />
-      </div>
-    );
+      <Container style={{marginTop: '4em'}}>
+        <Segment padded={'very'}>
+          <Header as='h2'>Notify Client</Header>
+          <Notification/>
+        </Segment>
+        <Segment padded={'very'}>
+          <Counter seed={10}/>
+        </Segment>
+      </Container>
+    )
   }
 }
 
@@ -31,4 +36,4 @@ class App extends Component {
     );
 
 */
-export default App;
+export default App
--- my-app/src/notification.jsx
+++ my-app/src/notification.jsx
@@ -1,54 +1,56 @@
-import React from "react";
-import request from "superagent";
+import React from 'react'
+import request from 'superagent'
+import { Grid, Form, Input, Button } from 'semantic-ui-react'
 
 export default class Notification extends React.Component {
-  constructor() {
-    super();
-    this.state = { message: "" };
+  constructor () {
+    super()
+    this.state = {message: ''}
 
-    this.handleMessageInput = this.handleMessageInput.bind(this);
-    this.handleNotify = this.handleNotify.bind(this);
+    this.handleMessageInput = this.handleMessageInput.bind(this)
+    this.handleNotify = this.handleNotify.bind(this)
   }
 
-  handleMessageInput(event) {
+  handleMessageInput (event) {
     // let obj = new Object()
     //let obj1 = {} // object literal
-    console.log(event.target.value);
-    this.setState({ message: event.target.value });
+    console.log(event.target.value)
+    this.setState({message: event.target.value})
   }
 
-  handleNotify(event) {
+  handleNotify (event) {
     // http://localhost:3000/notify?message=hello%20world
     request
-      .get("http://localhost:3000/notify")
+      .get('http://localhost:3000/notify')
       .query({
         message: this.state.message,
-        url: "http://www.google.com"
+        url: 'http://www.google.com'
       })
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) {
-          console.error(err);
-          return;
+          console.error(err)
+          return
         }
-        console.log("Response", res.text);
-      });
+        console.log('Response', res.text)
+      })
   }
-  render() {
+
+  render () {
     return (
-      <div>
-        <div>
-          <input
+      <Form>
+        <Form.Group widths={'equal'}>
+          <Input
             id="message"
             type="text"
             placeholder="Enter a message"
             onChange={this.handleMessageInput}
           />
-        </div>
+        </Form.Group>
         <div>
-          <button onClick={this.handleNotify}>Notify</button>
+          <Button onClick={this.handleNotify}>Notify</Button>
         </div>
-      </div>
-    );
+      </Form>
+    )
   }
 }
 
